d52d71d880cb7874ac3db14732600d7debaa9d41
tm10ymhp <tm10ymhp@protonmail.com>
2023-03-01 06:15:37 -0500

    comment min_modes and change map format

--- nvim/lua/tm10ymhp/maps.lua
+++ nvim/lua/tm10ymhp/maps.lua
@@ -28,7 +28,7 @@ vim.keymap.set('n', '<leader>b', ':Telescope buffers<cr>', opts)
 vim.keymap.set('n', '<leader>g', ':Telescope live_grep<cr>', opts)
 
 vim.keymap.set('n', '<leader>d', vim.lsp.buf.definition, opts)
-vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, opts)
+vim.keymap.set('n', '<leader>=', vim.lsp.buf.format, opts)
 vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
