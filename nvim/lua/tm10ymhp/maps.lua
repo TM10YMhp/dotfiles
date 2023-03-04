3568873e39949a6af7449915e6e279711365530e
tm10ymhp <tm10ymhp@protonmail.com>
2023-03-04 13:52:10 -0500

    041352

--- nvim/lua/tm10ymhp/maps.lua
+++ nvim/lua/tm10ymhp/maps.lua
@@ -1,3 +1,13 @@
+vim.keymap.set('n', 'Z', '<nop>')
+vim.keymap.set('n', 'ZZ', '<nop>')
+vim.keymap.set('n', 'ZQ', '<nop>')
+vim.keymap.set('n', '<c-z>', '<nop>')
+
+vim.keymap.set('n', [[\\]], [[:%s///gc<left><left><left>]])
+vim.keymap.set('n', [[\s]], ':LspStart')
+vim.keymap.set('n', [[\r]], ':LspRestart')
+vim.keymap.set('n', [[\i]], ':LspInfo')
+
 local opts = { noremap = true, silent = true }
 
 --[[move in wrap lines
@@ -9,26 +19,16 @@ vim.keymap.set('i', '<up>', '<c-o>gk', opts)
 vim.keymap.set('i', '<down>', '<c-o>gj', opts)
 ]]
 
-vim.keymap.set('n', 'Z', '<nop>', opts)
-vim.keymap.set('n', 'ZZ', '<nop>', opts)
-vim.keymap.set('n', 'ZQ', '<nop>', opts)
-vim.keymap.set('n', '<c-z>', '<nop>', opts)
-
 vim.keymap.set('n', '<tab>', '<cmd>bnext<cr>', opts)
 vim.keymap.set('n', '<s-tab>', '<cmd>bprevious<cr>', opts)
-vim.keymap.set('n', '\\', [[:%s///gc<left><left><left>]], opts)
-
-vim.keymap.set('n', [[\s]], ':LspStart', { noremap = true })
-vim.keymap.set('n', [[\r]], ':LspRestart', { noremap = true })
-vim.keymap.set('n', [[\i]], ':LspInfo', { noremap = true })
 
 vim.keymap.set('n', '<leader>h', '<cmd>nohls<cr>', opts)
 vim.keymap.set('n', '<leader>o', '<cmd>set list!<cr>', opts)
 
-vim.keymap.set('n', '<leader>d', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-vim.keymap.set('n', '<leader>=', '<cmd>lua vim.lsp.buf.format()<cr>', opts)
-vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+vim.keymap.set('n', '<leader>d', vim.lsp.buf.definition, opts)
+vim.keymap.set('n', '<leader>=', vim.lsp.buf.format, opts)
+vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
 
-vim.keymap.set('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
-vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
-vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
