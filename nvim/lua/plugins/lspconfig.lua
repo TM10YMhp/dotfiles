af0de39d9fe04bbba2905308d06ca9cf3494acbb
tm10ymhp <tm10ymhp@protonmail.com>
2023-03-07 16:47:33 -0500

    071647

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -84,7 +84,7 @@ return {
         },
       },
       settings = {
-        --completions = { completeFunctionCalls = true }
+        --completions = { completeFunctionCalls = true },
       }
     }
 
