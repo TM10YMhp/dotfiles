54aa9cdb0fa69ada8b4ca0d03a67a6150ef88525
Alejandro Maturrano <tm10ymhp@protonmail.com>
2024-06-09 18:38:13 -0500

    Update flexprompt_config.lua

--- clink/flexprompt_config.lua
+++ clink/flexprompt_config.lua
@@ -1,3 +1,11 @@
+local function my_first_module(args)
+  return "\n", "0"
+end
+flexprompt.add_module("mfm", my_first_module)
+
+flexprompt = flexprompt or {}
+flexprompt.settings = flexprompt.settings or {}
+
 flexprompt.settings.style = "lean"
 flexprompt.settings.frame_color = {
   "0", -- frame color
@@ -5,19 +13,23 @@ flexprompt.settings.frame_color = {
   "0", -- text color
   "0", -- separator color
 }
+flexprompt.settings.use_color_emoji = false
 flexprompt.settings.charset = "ascii"
-flexprompt.settings.lines = "two"
+flexprompt.settings.lines = "one"
 flexprompt.settings.use_icons = false
 flexprompt.settings.use_home_tilde = true
 flexprompt.settings.connection = "disconnected"
 flexprompt.settings.spacing = "sparse"
 flexprompt.settings.flow = "concise"
-flexprompt.settings.left_prompt = "{time:format=[%y.%m%d.%H%M|%S]}{git:nountracked:nostaged:noaheadbehind}{cwd:type=full}"
+
+-- HACK: use format of time to avoid prompt truncation
+flexprompt.settings.left_prompt = "{time}{git}{cwd}{mfm}"
 flexprompt.settings.right_prompt = ""
 flexprompt.settings.symbols.prompt = ">"
 flexprompt.settings.prompt_symbol_color = "0"
 flexprompt.settings.lean_separators = " "
-flexprompt.defaultargs["time"] = "color=0"
-flexprompt.defaultargs["git"] = "color=0:clean=0:conflict=0:dirty=0:remote=0:staged=0:unknown=0:unpublished=0"
-flexprompt.defaultargs["cwd"] = "color=0,0:rootcolor=0"
-flexprompt.settings.use_color_emoji = false
+
+flexprompt.defaultargs["time"] = "color=0:format=[%y.%m%d.%H%M|%S]"
+flexprompt.defaultargs["git"] =
+  "nountracked:nostaged:noaheadbehind:color=0:clean=0:conflict=0:dirty=0:remote=0:staged=0:unknown=0:unpublished=0"
+flexprompt.defaultargs["cwd"] = "type=full:color=0,0:rootcolor=0"
